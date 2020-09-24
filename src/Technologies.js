b247eb398ba6e65bb65a5cbf2a08615e4ba358c8
Postoian <Postoian@DT071.dinternal.com.ua>
2020-09-24 13:08:45 +0300

    delete unnecessary comments

--- src/Technologies.js
+++ src/Technologies.js
@@ -4,9 +4,6 @@ import logo from './logo.svg';
 const Technologies = () => {
     return ( 
     <div className = "App-header" >
-        {/* <div > first name </div> 
-        <img src = { logo } className = "App-logo" alt = "logo"/>
-        <a className = "App-link" href = "https://reactjs.org" target = "_blank" rel = "noopener noreferrer" ></a>  */}
         <ul>
         <li> react </li> 
         <li> angular </li> 
