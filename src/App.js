d015bf94e5109d09f3d46499742000dfdd5af367
Konstantin Postoian <hamelltonn@gmail.com>
2020-09-29 00:04:24 +0300

    add new pages-component Music/News/Settings

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import classes from '*.module.css'
 import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
 import './App.css'
 import Dialogs from './components/Dialogs/Dialogs'
+import Music from './components/Music/Music'
+import News from './components/News/News'
+import Settings from './components/Settings/Settings'
 import Header from './components/Header/Header'
 import Navbar from './components/Navbar/Navbar'
 import Profile from './components/Profile/Profile'
 
 const App = () => {
     return (
-        <div className='app-wrapper'>
-            <Header />
-            <Navbar />
-            <div class='app-wrapper-content'>
-                <Dialogs />
-            </div>
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header />
+                <Navbar />
+                <div class='app-wrapper-content'>
+                    <Route path='/profile' component={Profile} />
+                    <Route path='/dialogs' component={Dialogs} />
+                    <Route path='/news' component={News} />
+                    <Route path='/music' component={Music} />
+                    <Route path='/settings' component={Settings} />
 
-            {/* <Profile /> */}
-        </div>
+                </div>
+
+
+            </div>
+        </BrowserRouter>
     );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,10 +12,11 @@ import classes from './Navbar.module.css'
  const Navbar = () => {
      return(
         <nav className={classes.nav}>
-        <div className={`${classes.item}`}><a>Profile</a></div>
-        <div className={`${classes.item} ${classes.active}`}><a>Messages</a></div>
-        <div className={`${classes.item}`}><a>News</a></div>
-        <div className={`${classes.item}`}><a>Music</a></div>
+        <div className={`${classes.item}`}><a href="/profile">Profile</a></div>
+        <div className={`${classes.item} ${classes.active}`}><a href="/dialogs">Messages</a></div>
+        <div className={`${classes.item}`}><a href="/news">News</a></div>
+        <div className={`${classes.item}`}><a href="/music">Music</a></div>
+        <div className={`${classes.item}`}><a href="/settings">Settings</a></div>
     </nav>
  )
  }
