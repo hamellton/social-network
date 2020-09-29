1a621a91a735784bb7452e7cdda886fe144e546a
unknown <hamelltonn@gmail.comm>
2020-09-29 19:18:47 +0300

    change tag <a> on <NavLink> for cencel refresh page when clicking on the link, add style for active link

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Header from './components/Header/Header'
 import Navbar from './components/Navbar/Navbar'
 import Profile from './components/Profile/Profile'
 
-const App = () => {
+const App = (props) => {
     return (
         <BrowserRouter>
             <div className='app-wrapper'>
