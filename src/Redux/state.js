ec497fc5cb6d12fbc9229bf2796a7e5e3cdc63e7
hamellton <hamelltonn@gmail.com>
2020-11-19 16:45:58 +0300

    create store for state (use OOP programming), rename few function, use context for BLL and use method bind for tight binding 'this', add privat key and methods in store, delete unnecessary files (logo, setupTest, app.test)

--- src/Redux/state.js
+++ src/Redux/state.js
@@ -1,60 +1,65 @@
-// import uuid from 'react-uuid'  // for autogeneration id
 
-let rerenderEntireTree = () => {
-console.log('state changed')
-}
-
-let idPost = 3
-
-let state = {
-    profilePage: {
-        posts : [
-            { id: 1, message: 'it\'s my first post', likesCounter: 64 },
-            { id: 2, message: 'it\'s my second post', likesCounter: 58 }
-          ]
+// import { v4 as uuidv4 } from 'uuid';  // for autogeneration id
+let store = {
+    _idPosts: {
+        id: 3,
     },
-    dialogsPage : {
-        messagesData : [
-            { id: 1, message: 'Yo' },
-            { id: 2, message: 'When start new webinar?' },
-            { id: 3, message: 'How your new pages on React?' },
-            { id: 4, message: 'New message from Viktor' }
-        ],
-        dialogsData : [
-            { id: 1, name: 'Kostya' },
-            { id: 2, name: 'Vladilen' },
-            { id: 3, name: 'Masha' },
-            { id: 4, name: 'Viktor' }
-        ]
+    _state: {
+        profilePage: {
+            posts : [
+                { id: 1, message: 'it\'s my first post', likesCounter: 64 },
+                { id: 2, message: 'it\'s my second post', likesCounter: 58 }
+              ]
+        },
+        dialogsPage : {
+            messagesData : [
+                { id: 1, message: 'Yo' },
+                { id: 2, message: 'When start new webinar?' },
+                { id: 3, message: 'How your new pages on React?' },
+                { id: 4, message: 'New message from Viktor' }
+            ],
+            dialogsData : [
+                { id: 1, name: 'Kostya' },
+                { id: 2, name: 'Vladilen' },
+                { id: 3, name: 'Masha' },
+                { id: 4, name: 'Viktor' }
+            ]
+        },
+        sideBar : {
+            sidebar : [
+                {id: 1, name: 'Dasha'},
+                {id: 2, name: 'Pasha'},
+                {id: 3, name: 'Karina'}
+            ]
+        }
     },
-    sideBar : {
-        sidebar : [
-            {id: 1, name: 'Dasha'},
-            {id: 2, name: 'Pasha'},
-            {id:3, name: 'Karina'}
-        ]
+    getState() {
+        return this._state
+    },
+    _callSubscriber() {
+        console.log('state changed')
+    },
+    addPost(postsMessage) {
+            let newPost = {
+            id: this._idPosts.id++,
+            message: postsMessage,
+            likesCounter: 0
+        }
+        this._state.profilePage.posts.push(newPost)
+        // console.log(this._state.profilePage.posts)
+        this._callSubscriber(this._state)
+    },
+    deletePost() {
+        this._state.profilePage.posts.pop()
+        this._callSubscriber(this._state)
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer //observer слушатель, паттерн
     }
 }
-window.state = state
 
-export const addPost = (postsMessage) => {
-        let newPost = {
-        id: idPost++,
-        message: postsMessage,
-        likesCounter: 0
-    }
-    state.profilePage.posts.push(newPost)
-    rerenderEntireTree(state)
-}
+window.store = store
 
-export const deletePost = () => {
-    state.profilePage.posts.pop()
-    rerenderEntireTree(state)
-}
-
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer //observer слушатель, паттерн
-}
-
-export default state
+export default store
+// STORE = OOP!!!
 
--- src/index.js
+++ src/index.js
@@ -2,19 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import state, { addPost, deletePost, subscribe } from './Redux/state'
+import store from './Redux/state'
 import { BrowserRouter } from 'react-router-dom'
 
 let rerenderEntireTree = (state) => {
   ReactDOM.render(
     <BrowserRouter>
-      <App state={state} addPost={addPost} deletePost={deletePost} />
+      <App state={state} addPost={store.addPost.bind(store)} deletePost={store.deletePost.bind(store)} />
       </BrowserRouter>
     ,
     document.getElementById('root')
   );
 }
+rerenderEntireTree(store.getState())
 
-rerenderEntireTree(state)
-
-subscribe(rerenderEntireTree)
+store.subscribe(rerenderEntireTree)
